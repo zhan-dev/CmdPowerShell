d4b71123301a5041bc1096519513245da6e1a7bf
Zhan <Zhan.Kariahin@gmail.com>
2025-04-03 20:46:14 +0300

    added DataSave option for result and history, code improvements

--- CmdPowerShell/main.cs
+++ CmdPowerShell/main.cs
@@ -17,42 +17,61 @@ namespace CmdPowerShell
         public main()
         {
             InitializeComponent();
+
+            historyDownload_btn.Click += (sender, a) => DataSave("history.txt", history_lbl.Text);
+            resultDownload_btn.Click += (sender, a) => DataSave("result.txt", result_txt.Text);
         }
 
         private bool isEnterPressed = false;
 
         private void cmd_btn_Click(object sender, EventArgs e)
         {
-            cmd_txt.Text = "ipconfig";
-            ThroughPowerShellOrCmd("cmd", "/c", cmd_txt.Text);
+            //cmd_txt.Text = "ipconfig";
+            if (cmd_txt.Text != "")
+            {
+                ThroughPowerShellOrCmd("cmd", "/c", cmd_txt.Text);
 
-            DataToArray();
+                DataToArray("cmd");
 
-            cmd_txt.Text = "";
-            cmd_txt.Focus();
+                cmd_txt.Text = "";
+                cmd_txt.Focus();
+            }
+            else
+            {
+                MessageBox.Show("в строке пустовато");
+                cmd_txt.Focus();
+            }
         }
 
         private void pwShell_btn_Click(object sender, EventArgs e)
         {
-            pwShell_txt.Text = "get-appxpackage | ft name";
-            ThroughPowerShellOrCmd("powershell", "/command", pwShell_txt.Text);
+            //pwShell_txt.Text = "get-appxpackage | ft name";
+            if (pwShell_txt.Text != "")
+            {
+                ThroughPowerShellOrCmd("powershell", "/command", pwShell_txt.Text);
 
-            DataToArray();
+                DataToArray("powerShell");
 
-            pwShell_txt.Text = "";
-            pwShell_txt.Focus();
+                pwShell_txt.Text = "";
+                pwShell_txt.Focus();
+            }
+            else 
+            { 
+                MessageBox.Show("в строке пустовато");
+                pwShell_txt.Focus();
+            }
         }
 
         int i = 0;
         string[] history = new string[0];
-        void DataToArray()
+        void DataToArray(string source)
         {
             result_txt.Text = "Result:";
             history_lbl.Text = "Request History:";
 
             //Resize the array to a bigger size(five elements larger).
             Array.Resize(ref history, history.Length + 1);
-            history[i] = $"{cmd_txt.Text}{pwShell_txt.Text}";
+            history[i] = source == "cmd" ? $"{cmd_txt.Text}" : $"{pwShell_txt.Text}";
             i++;
 
             ////удаление через реверс
@@ -92,7 +111,6 @@ namespace CmdPowerShell
             //последовательный вывод процесса работы вместо конечного результата
             process.BeginOutputReadLine();
             process.OutputDataReceived += (sender, e) => { result_txt.Text += e.Data + Environment.NewLine; }; // эквивалент \n
-            //process.OutputDataReceived += (sender, e) => { result_lbl.Text += e.Data + Environment.NewLine; }; // эквивалент \n
 
             //result_lbl.Text += process.StandardOutput.ReadToEnd(); //просто вывод конечного результата
         }
@@ -127,12 +145,31 @@ namespace CmdPowerShell
             isEnterPressed = false;
         }
 
-        private void historyDownload_btn_Click(object sender, EventArgs e)
+        void DataSave(string fileName, string data)
         {
-            string path = "C:\\Users\\slice\\Desktop";
+            try
+            {
+                string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, $"{fileName}");
 
-            //FileStream file = new FileStream();
+                if (!string.IsNullOrWhiteSpace(path))
+                {
+                    using (FileStream file = new FileStream(path, FileMode.OpenOrCreate))
+                    {
+                        using (StreamWriter stream = new StreamWriter(file))
+                        {
+                            string timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"); // Формат даты
+                            string content = $"[{timestamp}]{Environment.NewLine}{data}";
+                            stream.WriteLine(content);
 
+                            DialogResult result = MessageBox.Show("Cессия запросов сохранена \n \n Открываем папку с файлом?", "Результат",
+                                MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2);
+                            if (result == DialogResult.Yes)
+                                Process.Start("explorer.exe", Path.GetDirectoryName(path));
+                        }
+                    }
+                }
+            }
+            catch (Exception ex) { MessageBox.Show($"Ошибка сохранения: {ex.Message}"); }
         }
     }
 }
